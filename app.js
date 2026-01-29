2ef2be9d00ab777d0fedecb5712528fd578438c6
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-01-29 13:57:21 -0800

    made changes to app.js

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const app = express();
 // Define Port number where server will listen
 const PORT = 3000
 
+// Enable static file sharing
+app.use(express.static('public'));
+
 // DEFINING OUR MAIN ROUTE ('/')
 app.get('/', (req,res) => {
-res.send("Benvenuto a Pizza Da Michele");
+res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
 //Starting the server and listen on the designated port 
 app.listen(PORT,() => {
-    console.log(`Server is running at 
-        http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
